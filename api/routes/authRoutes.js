ab07063ecc3c23665d8b0a8c95e0f16bd9a16c77
MohamedDib <49766520+MohamedDib@users.noreply.github.com>
2021-10-16 17:22:40 +0200

    Cleanup api

--- api/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const {register,login,signOut} = require('../controllers/authController')
-
-const router = require("express").Router();
-
-router.get('/register', register)
-router.get('/login', login)
-router.get('/signOut', signOut)
-
-module.exports = router
