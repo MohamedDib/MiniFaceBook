4c51e079206fe3e12c007cb6074d658c23b091ab
MohamedDib <49766520+MohamedDib@users.noreply.github.com>
2021-10-18 01:45:05 +0200

    Update app.js

--- api/app.js
+++ api/app.js
@@ -25,10 +25,10 @@ const app = express();
 // Configuration cors
 app.use((req, res, next) => {
   //res.setHeader('Access-Control-Allow-Origin', 'https://paolibook.netlify.app');
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://paolibook.netlify.app');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  //res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
