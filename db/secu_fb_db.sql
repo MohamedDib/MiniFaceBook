096fd583eef6da57fcc8d59efa0bcd5f592a8435
MohamedDib <49766520+MohamedDib@users.noreply.github.com>
2021-10-17 18:00:45 +0200

    DB Update : include privacy directly to users table

--- db/secu_fb_db.sql
+++ db/secu_fb_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 16, 2021 at 03:25 PM
+-- Generation Time: Oct 17, 2021 at 03:55 PM
 -- Server version: 5.7.31
 -- PHP Version: 7.4.9
 
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `comments` (
   PRIMARY KEY (`id`),
   KEY `fk_Comments_Users_id` (`user_id`),
   KEY `fk_Comments_Posts_id` (`post_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `comments`
@@ -49,7 +49,8 @@ INSERT INTO `comments` (`id`, `content`, `user_id`, `post_id`, `comment_date`) V
 (12, 'Pas mal pour travailler au calme...', 13, 1, '2020-09-24 11:20:53'),
 (13, 'Omitto iuris dictionem in libera civitate contra leges senatusque consulta; caedes relinquo; libidines praetereo, quarum acerbissimum extat indicium et ad insignem memoriam turpitudinis et paene ad iustum odium imperii nostri, quod constat nobilissimas virgines se in puteos abiecisse et morte voluntaria necessariam turpitudinem depulisse. Nec haec idcirco omitto, quod non gravissima sint, sed quia nunc sine teste dico.', 13, 1, '2020-09-24 11:36:12'),
 (26, 'Très joli bleu!..', 13, 1, '2020-09-28 14:40:31'),
-(28, 'test', 16, 2, '2020-10-03 20:17:37');
+(28, 'test', 16, 2, '2020-10-03 20:17:37'),
+(29, 'Fuck', 21, 1, '2021-10-16 21:45:13');
 
 -- --------------------------------------------------------
 
@@ -63,7 +64,7 @@ CREATE TABLE IF NOT EXISTS `friends` (
   `id_user1` smallint(6) DEFAULT NULL,
   `id_user2` smallint(6) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `friends`
@@ -76,7 +77,11 @@ INSERT INTO `friends` (`id`, `id_user1`, `id_user2`) VALUES
 (4, 15, 16),
 (7, 21, 17),
 (8, 22, 15),
-(9, 22, 19);
+(9, 22, 19),
+(10, 21, 18),
+(11, 21, 14),
+(12, 21, 15),
+(13, 21, 25);
 
 -- --------------------------------------------------------
 
@@ -117,18 +122,26 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `picture` varchar(255) DEFAULT NULL,
   `description` text,
   `post_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `privacy_id` int(11) DEFAULT '1',
+  `privacy` varchar(250) DEFAULT '1',
   PRIMARY KEY (`id`),
   KEY `fk_Posts_Users_id` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `posts`
 --
 
-INSERT INTO `posts` (`id`, `user_id`, `picture`, `description`, `post_date`, `privacy_id`) VALUES
-(1, 11, 'http://localhost:3000/images/Walters_Work_1600805964762.png', 'I have spent my whole life scared, frightened of things that could happen, might happen, might not happen, 50 years I spent like that. Finding myself awake at three in the morning. But you know what? Ever since my diagnosis, I sleep just fine. What I came to realize is that fear, that’s the worst of it. That’s the real enemy. So, get up, get out in the real world and you kick that bastard as hard you can right in the teeth.', '2021-10-12 21:12:12', 1),
-(2, 13, 'http://localhost:3000/images/Walters_Work_1600853648683.png', 'I have spent my whole life scared, frightened of things that could happen, might happen, might not happen, 50 years I spent like that. Finding myself awake at three in the morning. But you know what? Ever since my diagnosis, I sleep just fine. What I came to realize is that fear, that’s the worst of it. That’s the real enemy. So, get up, get out in the real world and you kick that bastard as hard you can right in the teeth.', '2021-10-12 21:12:12', 1);
+INSERT INTO `posts` (`id`, `user_id`, `picture`, `description`, `post_date`, `privacy`) VALUES
+(1, 11, 'http://localhost:3000/images/Walter\'s_Work_1600805964762.png', 'I have spent my whole life scared, frightened of things that could happen, might happen, might not happen, 50 years I spent like that. Finding myself awake at three in the morning. But you know what? Ever since my diagnosis, I sleep just fine. What I came to realize is that fear, that’s the worst of it. That’s the real enemy. So, get up, get out in the real world and you kick that bastard as hard you can right in the teeth.', '2021-10-12 21:12:12', 'pv'),
+(2, 13, 'http://localhost:3000/images/Walter\'s_Work_1600853648683.png', 'I have spent my whole life scared, frightened of things that could happen, might happen, might not happen, 50 years I spent like that. Finding myself awake at three in the morning. But you know what? Ever since my diagnosis, I sleep just fine. What I came to realize is that fear, that’s the worst of it. That’s the real enemy. So, get up, get out in the real world and you kick that bastard as hard you can right in the teeth.', '2021-10-12 21:12:12', 'fr'),
+(7, 21, NULL, 'dezfezf ', '2021-10-16 22:47:28', 'fr'),
+(8, 21, NULL, 'nnnnnnn', '2021-10-16 22:47:53', 'fr'),
+(9, 21, NULL, 'fuuuuuuuuuuuuuuuuuuuuuuck', '2021-10-16 22:48:25', 'pv'),
+(10, 21, NULL, 'yess', '2021-10-16 22:48:37', 'pb'),
+(11, 21, NULL, 'My post content', '2021-10-16 22:55:41', 'pv'),
+(12, 21, NULL, 'azdazd', '2021-10-16 23:56:04', 'pb'),
+(13, 21, 'Hello fuckers', 'ddddd', '2021-10-16 23:56:56', 'pb'),
+(14, 21, NULL, 'zebzebzebzebzb', '2021-10-16 23:59:53', 'pb');
 
 -- --------------------------------------------------------
 
@@ -170,7 +183,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `picture` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `ind_uni_email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
@@ -186,7 +199,8 @@ INSERT INTO `users` (`id`, `name`, `email`, `tel`, `address`, `description`, `pa
 (18, 'Hatim Sadeq', 'hatima@email.com', NULL, NULL, NULL, 'U2FsdGVkX18t2Mvo8Wt0oW1ttG62F+4urstp+PedY62lLnLgXOsRO0wed53F1xYgyC9vEKzDfCpNlduiMgc/zhNnt9A38fjKvV0reiLIuSo=', NULL),
 (19, 'Hatim Sadeq', 'hatimas@email.com', NULL, NULL, NULL, 'U2FsdGVkX1+BEfjMI4n429kSOTzgZc4QnyQqPok5Dx+OYOxuYXXndAmEkhvkMH24cyix8vJxnlbmameJsEGjWQOrX2Ro/49SXytBdm35k38=', NULL),
 (21, 'user1', 'user1@email.com', NULL, NULL, NULL, 'U2FsdGVkX1/21yJGyVR9btJJP5BzcjxNmmzC0BkDMjfIJ/+X2Zn/flCtOn5U+ESEES94SFik1W56GftlqrEXzc0/vMpqc75ZF5e7fPD95Uk=', NULL),
-(22, 'user2', 'user2@email.com', NULL, NULL, NULL, 'U2FsdGVkX19ACBsYUb/uHq7wSwtsvjkEwklZd0eJ6f+gbPqIPn6brjdpa+/JqAvqz8hNo2C2/CXQnP4VMTbl3SXIk33FjOBEvvwR/QnkMGQ=', NULL);
+(22, 'user2', 'user2@email.com', NULL, NULL, NULL, 'U2FsdGVkX19ACBsYUb/uHq7wSwtsvjkEwklZd0eJ6f+gbPqIPn6brjdpa+/JqAvqz8hNo2C2/CXQnP4VMTbl3SXIk33FjOBEvvwR/QnkMGQ=', NULL),
+(25, 'User3', 'user3@email.com', NULL, NULL, NULL, 'U2FsdGVkX1/3yMuP59f+9ml0EOnSfIf0a/2bVcTHFQGYSg6Zcw6nW3lIi5XjG/Fbkz9F+Mp6+lyh29RiIQX5bHxe+t/wgR8oc/y7RC1mRak=', NULL);
 
 --
 -- Constraints for dumped tables
