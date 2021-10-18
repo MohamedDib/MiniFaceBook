16ebb32acfd312e80e40faebee0eb79bb1975394
MohamedDib <49766520+MohamedDib@users.noreply.github.com>
2021-10-18 10:37:57 +0200

    api link update en env.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backendServer: 'https://minifb-api.herokuapp.com/'
+  backendServer: 'https://minifb-api.herokuapp.com'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   //backendServer: 'http://localhost:3000'
-  backendServer: 'https://minifb-api.herokuapp.com/'
+  backendServer: 'https://minifb-api.herokuapp.com'
 };
 
 /*
