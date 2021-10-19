d5f19306e268314b1c961a550bf4af3d08ff9932
massinissa06 <massinfoo@gmail.com>
2021-10-19 15:47:01 +0200

    change server.js and package .js

--- frontend/server.js
+++ frontend/server.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static("./dist/frontend"));
+app.use(express.static(__dirname + '/dist/frontend'));
 
-app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "dist/frontend" });
+app.get('/*', (req,res,next) => {
+    res.sendFile(path.join(__dirname + '/dist/dictionary/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
 
-console.log(`Running on port ${process.env.PORT || 8080}`);
+app.listen(process.env.PORT || 8000);
