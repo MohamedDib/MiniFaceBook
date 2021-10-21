e8047da03ce58a771492849d26822de3a6b671ce
MohamedDib <49766520+MohamedDib@users.noreply.github.com>
2021-10-21 11:08:21 +0200

    Allow Credentials

--- api/app.js
+++ api/app.js
@@ -24,11 +24,11 @@ const app = express();
  */
 // Configuration cors
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://paolibook.herokuapp.com');
   //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  //res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
