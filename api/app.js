112f106279a1e76c34e7be8edd07bbe5b74be1f7
MohamedDib <49766520+MohamedDib@users.noreply.github.com>
2021-10-27 20:48:39 +0200

    Back to local

--- api/app.js
+++ api/app.js
@@ -25,8 +25,7 @@ const app = express();
 // Configuration cors
 app.use((req, res, next) => {
   //res.setHeader('Access-Control-Allow-Origin', 'https://paolibook.herokuapp.com');
-  //res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:4200');
-  res.setHeader('Access-Control-Allow-Origin', 'https://paolibook2.web.app');
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:4200');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   res.setHeader('Access-Control-Allow-Credentials', true);
